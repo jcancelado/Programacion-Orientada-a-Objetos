7657924dc765f82e7a9bb36fc3ad710c374dbc63
jcancelado <jcancelado@unal.edu.co>
2025-08-28 15:32:06 -0500

    Add files via upload

---
+++ ejercicio18.py
@@ -0,0 +1,6 @@
+#José Luis Cancelado Castro
+numero = int(input("Ingrese un número entero en formato númerico: "))
+for x in range(1, 101):
+    resultado = numero
+    resultado = numero + x
+print(resultado)
